c0a552fcaeac26a1047ded74c4243d400d300c70
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2025-05-02 01:51:29 +0200

    Minor update

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Home(){
   return (
     <>
-    There's nothing here. Navigate to <Link href="/todo">Todos</Link> instead.
+    There's nothing here. Navigate to <Link className="text-blue-500 font-bold" href="/todo">Todos</Link> instead.
     </>
   )
 }
