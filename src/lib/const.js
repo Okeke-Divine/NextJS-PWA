b253f958d03065c3ac1fed4cb64a09b2c198e2b7
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2025-05-06 23:59:27 +0200

    modiferd/ deleted shared/pwa-installbutton.jsx

--- src/lib/const.js
+++ src/lib/const.js
@@ -1,7 +1,7 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-export const APP_NAME = "Todo Apl";
-export const APP_DEFAULT_TITLE = "Todo Apl";
-export const APP_TITLE_TEMPLATE = "%s - TAPL";
-export const APP_DESCRIPTION = "best todo app on earth";
+export const APP_NAME = "NextJS PWA";
+export const APP_DEFAULT_TITLE = "Nextjs PWA";
+export const APP_TITLE_TEMPLATE = "%s - NextjsPWA";
+export const APP_DESCRIPTION = "best pwa app on earth";
 export const URL_TERMS_OF_SERVICE = "/tos";
 export const URL_PRIVACY_POLICY = "/privacy-policy"
